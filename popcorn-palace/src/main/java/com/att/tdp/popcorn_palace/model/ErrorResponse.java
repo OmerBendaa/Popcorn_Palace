f4f2faf5f6f9a581eb2664d1889c8e1be655bece
omerbenda <omerbd29@cs.colman.ac.il>
2025-03-24 20:59:57 +0200

    Data getters and setters

--- popcorn-palace/src/main/java/com/att/tdp/popcorn_palace/model/ErrorResponse.java
+++ popcorn-palace/src/main/java/com/att/tdp/popcorn_palace/model/ErrorResponse.java
@@ -1,10 +1,8 @@
 package com.att.tdp.popcorn_palace.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Setter
-@Getter
+@Data
 
 public class ErrorResponse {
     private String message;
--- popcorn-palace/src/main/java/com/att/tdp/popcorn_palace/model/Movie.java
+++ popcorn-palace/src/main/java/com/att/tdp/popcorn_palace/model/Movie.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import jakarta.persistence.GenerationType;
 
-@Getter
-@Setter
+@Data
 @Entity
 
 public class Movie {
