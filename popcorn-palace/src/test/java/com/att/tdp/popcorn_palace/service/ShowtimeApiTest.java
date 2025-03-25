2666be062524564ab48b76e856c5a575d07bc0c7
omerbenda <omerbd29@cs.colman.ac.il>
2025-03-25 16:54:56 +0200

    ShowtimeApiTest

--- popcorn-palace/src/test/java/com/att/tdp/popcorn_palace/service/ShowtimeApiTest.java
+++ popcorn-palace/src/test/java/com/att/tdp/popcorn_palace/service/ShowtimeApiTest.java
@@ -3,6 +3,7 @@ package com.att.tdp.popcorn_palace.service;
 import com.att.tdp.popcorn_palace.exception.NotFoundException;
 import com.att.tdp.popcorn_palace.model.Movie;
 import com.att.tdp.popcorn_palace.model.ShowTime;
+import com.att.tdp.popcorn_palace.repository.IBookingRepository;
 import com.att.tdp.popcorn_palace.repository.IMovieRepository;
 import com.att.tdp.popcorn_palace.repository.IShowTimeRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,11 +20,10 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -35,12 +35,16 @@ class ShowtimeApiTest {
     @Mock
     private IMovieRepository movieRepository;
 
+    @Mock
+    private IBookingRepository bookingRepository;
+
     @InjectMocks
     private ShowTimeService showTimeService;
 
     private Movie movie;
     private Movie secondMovie;
     private ShowTime showTime;
+    private ShowTime secondShowTime;
 
     @BeforeEach
     void setUp() {
@@ -63,6 +67,14 @@ class ShowtimeApiTest {
         showTime.setStartTime(Instant.now());
         showTime.setEndTime(Instant.now().plusSeconds(2 * 60 * 60));
         showTime.setPrice(10.0);
+
+        secondShowTime = new ShowTime();
+        secondShowTime.setId(2L);
+        secondShowTime.setMovieId(2L);
+        secondShowTime.setTheater("Theater 2");
+        secondShowTime.setStartTime(Instant.now());
+        secondShowTime.setEndTime(Instant.now().plusSeconds(3 * 60 * 60));
+        secondShowTime.setPrice(10.0);
 
         when(movieRepository.save(movie)).thenReturn(movie);
         when(movieRepository.save(secondMovie)).thenReturn(secondMovie);
@@ -304,5 +316,183 @@ class ShowtimeApiTest {
             assertEquals("The showTime you are trying to add overlaps with an existing showTime",
                     exception.getMessage());
         }
+    }
+
+    @Test
+    void updateSuccessTest() {
+        ShowTime updatedShowTime = new ShowTime();
+        updatedShowTime.setId(1L);
+        updatedShowTime.setMovieId(2L);
+        updatedShowTime.setTheater("Theater 1");
+        updatedShowTime.setStartTime(showTime.getStartTime());
+        updatedShowTime.setEndTime(showTime.getStartTime().plusSeconds(3 * 60 * 60));
+        updatedShowTime.setPrice(10.0);
+
+        when(showTimeRepository.findById(updatedShowTime.getId())).thenReturn(Optional.of(showTime));
+        when(movieRepository.findById(updatedShowTime.getMovieId())).thenReturn(Optional.of(secondMovie));
+        when(showTimeRepository.save(updatedShowTime)).thenReturn(updatedShowTime);
+        showTimeService.updateShowTime(updatedShowTime.getId(), updatedShowTime);
+
+        when(showTimeRepository.findById(updatedShowTime.getId())).thenReturn(Optional.of(updatedShowTime));
+        ShowTime result = showTimeRepository.findById(updatedShowTime.getId()).get();
+        assertEquals(updatedShowTime.getMovieId(), result.getMovieId());
+        assertEquals(updatedShowTime.getEndTime(), result.getEndTime());
+        assertEquals(updatedShowTime.getTheater(), result.getTheater());
+        assertEquals(updatedShowTime.getStartTime(), result.getStartTime());
+        assertEquals(updatedShowTime.getPrice(), result.getPrice());
+
+    }
+
+    @Test
+    void updateWithNotExistingShowtimeTest() {
+        ShowTime updatedShowTime = new ShowTime();
+        updatedShowTime.setId(10L);
+        updatedShowTime.setMovieId(1L);
+        updatedShowTime.setTheater("Theater 1");
+        updatedShowTime.setStartTime(Instant.now());
+        updatedShowTime.setEndTime(Instant.now().plusSeconds(2 * 60 * 60));
+        updatedShowTime.setPrice(10.0);
+        when(showTimeRepository.findById(updatedShowTime.getId())).thenReturn(Optional.empty());
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+            showTimeService.updateShowTime(updatedShowTime.getId(), updatedShowTime);
+        });
+        assertEquals("There is no showTime with the given Id '10'", exception.getMessage());
+    }
+
+    @Test
+    void updateFailNegativeMovieIdTest() {
+        ShowTime updatedShowTime = new ShowTime();
+        updatedShowTime.setId(10L);
+        updatedShowTime.setMovieId(-1L);
+        updatedShowTime.setTheater("Theater 1");
+        updatedShowTime.setStartTime(Instant.now());
+        updatedShowTime.setEndTime(Instant.now().plusSeconds(2 * 60 * 60));
+        updatedShowTime.setPrice(10.0);
+
+        when(showTimeRepository.findById(updatedShowTime.getId())).thenReturn(Optional.of(showTime));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            showTimeService.updateShowTime(updatedShowTime.getId(), updatedShowTime);
+        });
+        assertEquals("movieId must be a valid id - greater than 0", exception.getMessage());
+    }
+
+    @Test
+    void updateFailNonExistingMovieTest() {
+        ShowTime updatedShowTime = new ShowTime();
+        updatedShowTime.setId(1L);
+        updatedShowTime.setMovieId(90L);
+        updatedShowTime.setTheater("Theater 1");
+        updatedShowTime.setStartTime(Instant.now());
+        updatedShowTime.setEndTime(Instant.now().plusSeconds(2 * 60 * 60));
+        updatedShowTime.setPrice(10.0);
+
+        when(showTimeRepository.findById(updatedShowTime.getId())).thenReturn(Optional.of(showTime));
+        when(movieRepository.findById(updatedShowTime.getMovieId())).thenReturn(Optional.empty());
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+            showTimeService.updateShowTime(updatedShowTime.getId(), updatedShowTime);
+        });
+        assertEquals("There is no movie with the given id '90'", exception.getMessage());
+    }
+
+    @Test
+    void partialUpdateSuccessTest() {
+        ShowTime partialUpdateShowTime = new ShowTime();
+        partialUpdateShowTime.setId(1L);
+        partialUpdateShowTime.setTheater("Theater 5");
+        partialUpdateShowTime.setPrice(15.0);
+
+        when(showTimeRepository.findById(partialUpdateShowTime.getId())).thenReturn(Optional.of(showTime));
+        when(movieRepository.findById(showTime.getMovieId())).thenReturn(Optional.of(movie));
+        when(showTimeRepository.save(showTime)).thenReturn(showTime);
+
+        showTimeService.updateShowTime(partialUpdateShowTime.getId(), partialUpdateShowTime);
+
+        showTime.setTheater(partialUpdateShowTime.getTheater());
+        showTime.setPrice(partialUpdateShowTime.getPrice());
+        when(showTimeRepository.findById(partialUpdateShowTime.getId())).thenReturn(Optional.of(showTime));
+        ShowTime result = showTimeRepository.findById(partialUpdateShowTime.getId()).get();
+
+        assertEquals(partialUpdateShowTime.getTheater(), result.getTheater());
+        assertEquals(partialUpdateShowTime.getPrice(), result.getPrice());
+        assertEquals(showTime.getMovieId(), result.getMovieId());
+        assertEquals(showTime.getStartTime(), result.getStartTime());
+        assertEquals(showTime.getEndTime(), result.getEndTime());
+    }
+
+    @Test
+    void updateFailStartTimeAfterEndTimeTest() {
+        ShowTime updatedShowTime = new ShowTime();
+        updatedShowTime.setId(1L);
+        updatedShowTime.setMovieId(1L);
+        updatedShowTime.setTheater("Theater 1");
+        updatedShowTime.setStartTime(Instant.now().plusSeconds(2 * 60 * 60)); // Set startTime after endTime
+        updatedShowTime.setEndTime(Instant.now());
+        updatedShowTime.setPrice(10.0);
+
+        when(showTimeRepository.findById(updatedShowTime.getId())).thenReturn(Optional.of(showTime));
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            showTimeService.updateShowTime(updatedShowTime.getId(), updatedShowTime);
+        });
+
+        assertEquals("startTime must be earlier than endTime", exception.getMessage());
+    }
+
+    @Test
+    void updateFailScreeningTimeTooShortTest() {
+        ShowTime updatedShowTime = new ShowTime();
+        updatedShowTime.setId(1L);
+        updatedShowTime.setMovieId(2L);
+        updatedShowTime.setTheater("Theater 1");
+        updatedShowTime.setStartTime(Instant.now());
+        updatedShowTime.setEndTime(Instant.now().plusSeconds(2 * 60 * 60));
+        updatedShowTime.setPrice(10.0);
+
+        when(showTimeRepository.findById(updatedShowTime.getId())).thenReturn(Optional.of(showTime));
+        when(movieRepository.findById(updatedShowTime.getMovieId())).thenReturn(Optional.of(secondMovie));
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            showTimeService.updateShowTime(updatedShowTime.getId(), updatedShowTime);
+        });
+        assertEquals("The duration of a showtime must be equal or greater than the movie's duration",
+                exception.getMessage());
+    }
+
+    @Test
+    void updateFailShowtimeOverlapsesTest() {
+        // Update secondShowTime to have the same theater and overlapping time with the
+        // first showTime
+        secondShowTime.setTheater("Theater 1");
+        secondShowTime.setStartTime(showTime.getStartTime().plusSeconds(30 * 60)); // Overlapping time
+        secondShowTime.setEndTime(showTime.getEndTime().plusSeconds(30 * 60 * 60 * 60)); // Overlapping time
+
+        when(showTimeRepository.findById(secondShowTime.getId())).thenReturn(Optional.of(secondShowTime));
+        when(movieRepository.findById(secondShowTime.getMovieId())).thenReturn(Optional.of(secondMovie));
+        when(showTimeRepository.findOverlappingShowTimes(secondShowTime.getTheater(), secondShowTime.getStartTime(),
+                secondShowTime.getEndTime())).thenReturn(Arrays.asList(showTime));
+
+        DataIntegrityViolationException exception = assertThrows(DataIntegrityViolationException.class, () -> {
+            showTimeService.updateShowTime(secondShowTime.getId(), secondShowTime);
+        });
+
+        assertEquals("The updated showTime overlaps with an existing showTime", exception.getMessage());
+    }
+
+    @Test
+    void deleteShowTimeTest() {
+        when(showTimeRepository.findById(showTime.getId())).thenReturn(Optional.of(showTime));
+        when(showTimeRepository.existsById(showTime.getId())).thenReturn(true);
+
+        showTimeService.deleteShowTimeById(showTime.getId());
+
+        verify(showTimeRepository, times(1)).delete(showTime);
+        when(showTimeRepository.findById(showTime.getId())).thenReturn(Optional.empty());
+        when(showTimeRepository.existsById(showTime.getId())).thenReturn(false);
+
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+            showTimeService.deleteShowTimeById(showTime.getId());
+        });
+
+        assertEquals("There is no showTime with the given id '1'", exception.getMessage());
     }
 }
